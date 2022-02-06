70316629c6bc3153a34eec9cab3d1cf5d8aca144
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-05 16:36:02 -0800

    completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+// Solution 1
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Solution 2
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+
+// Solution 3
+let destructivelyPrependCat = (name) => cats.unshift(name);
+
+// Solution 4
+let destructivelyRemoveLastCat = () => cats.pop();
+
+// Solution 5
+let destructivelyRemoveFirstCat = () => cats.shift();
+
+// Solution 6
+function appendCat(name) {
+  return [...cats, name];
+}
+
+// Solution 7
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+// Solution 8
+let removeLastCat = () => cats.slice(0, -1);
+
+// Solution 9
+let removeFirstCat = () => cats.slice(1);
